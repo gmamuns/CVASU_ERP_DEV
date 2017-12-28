749d1e6fea622159a9e0fc33d86ff2c1cbc6d8f0
Md Mamun Sikder <gmamuns@gmail.com>
2017-12-28 18:42:58 +0600

    Add files via upload
    Connect and check DBBL payment  .... (ok)

---
+++ src/java/utils/Basic_Authentication_rocket.java
@@ -0,0 +1,54 @@
+//Worked (ok)
+
+//Requirement : "Common-codec-1.10.jar" (https://mvnrepository.com/artifact/commons-codec/commons-codec/1.10)
+//
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Basic_Authentication {
+
+	public static void main(String[] args) {
+
+		try {
+			String webPage = "http://103.11.136.153/BillPayGWT/BillInfoService?shortcode=555&userid=cfdgc101&password=e43dft4vdytrtht&opcode=GT&txnid=23002451";
+			String name = "dbill";
+			String password = "dBILL!23";
+
+			String authString = name + ":" + password;
+			System.out.println("auth string: " + authString);
+			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+			String authStringEnc = new String(authEncBytes);
+			System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+			URL url = new URL(webPage);
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+			InputStream is = urlConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			while ((numCharsRead = isr.read(charArray)) > 0) {
+				sb.append(charArray, 0, numCharsRead);
+			}
+			String result = sb.toString();
+
+			System.out.println("*** BEGIN ***");
+			System.out.println(result);
+			System.out.println("*** END ***");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
